ef0e80828479acf93bf6d4cd4cfe600620162b24
DonskykhKolya <43479699+DonskykhKolya@users.noreply.github.com>
2018-09-26 00:09:14 +0300

    Update GroupList.java

--- GroupList.java
+++ GroupList.java
@@ -3,17 +3,17 @@ package com.gmail.ndonskih63;
 public class GroupList {
 
 	public static void main(String[] args) throws DontMoreException {
-		Student st1 = new Student("Marhotska ", "Anastasia", 21, "female", 3, 108, 95);
-		Student st2 = new Student("Yurchenko ", "Anna", 20, "female", 3, 110, 63);
-		Student st3 = new Student("Bashkatova ", "Yana", 20, "female", 3, 101, 70);
-		Student st4 = new Student("Kolmakova ", "Inna", 20, "female", 3, 107, 82);
-		Student st5 = new Student("Donskikh ", "Nikolay", 20, "male", 3, 103, 88);
-		Student st6 = new Student("Kapitanchuk ", "Anastasia", 20, "female", 3, 106, 69);
-		Student st7 = new Student("Gnilitskaya ", "Darya", 20, "female", 3, 102, 85);
-		Student st8 = new Student("Zherebylo ", "Tatyana", 20, "female", 3, 105, 53);
-		Student st9 = new Student("Shastina ", "Anna", 20, "female", 3, 109, 65);
-		Student st10 = new Student("Zhalkovskaya ", "Alla", 21, "female", 3, 104, 63);
-		Student st11 = new Student("Sheynoga ", "Stanislav", 20, "male", 3, 111, 40);
+		Student st1 = new Student("Marhotska", "Anastasia", 21, "female", 3, 108, 95);
+		Student st2 = new Student("Yurchenko", "Anna", 20, "female", 3, 110, 63);
+		Student st3 = new Student("Bashkatova", "Yana", 20, "female", 3, 101, 70);
+		Student st4 = new Student("Kolmakova", "Inna", 20, "female", 3, 107, 82);
+		Student st5 = new Student("Donskikh", "Nikolay", 20, "male", 3, 103, 88);
+		Student st6 = new Student("Kapitanchuk", "Anastasia", 20, "female", 3, 106, 69);
+		Student st7 = new Student("Gnilitskaya", "Darya", 20, "female", 3, 102, 85);
+		Student st8 = new Student("Zherebylo", "Tatyana", 20, "female", 3, 105, 53);
+		Student st9 = new Student("Shastina", "Anna", 20, "female", 3, 109, 65);
+		Student st10 = new Student("Zhalkovskaya", "Alla", 21, "female", 3, 104, 63);
+		Student st11 = new Student("Sheynoga", "Stanislav", 20, "male", 3, 111, 40);
 		Group myGroup = new Group();
 
 		myGroup.addStudent(st1);
@@ -36,19 +36,19 @@ public class GroupList {
 		}
 		System.out.println();
 
-		System.out.println("Surname sorting, simple order:");
+		System.out.println("Surname sorting:");
 		myGroup.sortStudents(ItStudent.surname, true);
 		System.out.println(myGroup);
 		System.out.println();
-		System.out.println("Age sorting, reverse order:");
+		System.out.println("Age sorting:");
 		myGroup.sortStudents(ItStudent.age, false);
 		System.out.println(myGroup);
-		System.out.println("AverageScore sorting, simple order:");
+		System.out.println("AverageScore sorting:");
 		myGroup.sortStudents(ItStudent.averageScore, true);
 		System.out.println(myGroup);
 		System.out.println();
 
-		myGroup.deleteStudent("Zherebylo ");
+		myGroup.deleteStudent("Zherebylo");
 		myGroup.sortStudents(ItStudent.surname, true);
 		System.out.println(myGroup);
 
@@ -56,12 +56,16 @@ public class GroupList {
 		myGroup.sortStudents(ItStudent.surname, true);
 		System.out.println();
 		System.out.println(myGroup);
-
+		
+		myGroup.saveToFile();
+		System.out.println();
+		
 		ForVoenkom kom = new ForVoenkom();
 		for (Student st : kom.recruits(myGroup)) {
 			System.out.println(st);
-			System.out.println("You are waiting for a Voenkom!");
+			System.out.println("You are waiting for a Voenkom! \n");
 		}
+		myGroup.readFile("MyGroup.doc");
 
 	}
 
